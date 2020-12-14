b11fe2b4b95cb7ea981aece7128e1cb6d4946766
OlumideOjoye <74067873+OlumideOjoye@users.noreply.github.com>
2020-12-14 10:17:57 +0000

    Add files via upload

---
+++ Hadoop_install.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+
+echo "HADOOP INSTALLATION"
+
+cd ~/opt
+
+
+# Remove any the download if exists
+rm -f hadoop-2.7.3.tar.gz
+
+# Download from url
+echo "Downloading from URL..."
+wget http://archive.apache.org/dist/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz
+
+# Unzip file
+echo "Unzipping file..."
+tar -zxf hadoop-2.7.3.tar.gz
+
+# Remove zip file
+rm hadoop-2.7.3.tar.gz
+
+# Assign all admin rights 
+cd
+chmod 777 opt/
+cd opt
+chmod 777 hadoop-2.7.3
+cd hadoop-2.7.3
+mkdir hdfs
+cd hdfs
+mkdir datanode namenode
+
+cd
+
+
+# Edit .bash_profile 
+echo "Adding settings to .bash_profile file"
+
+echo '
+## HADOOP_HOME
+export HADOOP_HOME=~/opt/hadoop-2.7.3
+export HADOOP_INSTALL=$HADOOP_HOME
+export HADOOP_MAPRED_HOME=$HADOOP_HOME
+export HADOOP_COMMON_HOME=$HADOOP_HOME
+export HADOOP_HDFS_HOME=$HADOOP_HOME
+export YARN_HOME=$HADOOP_HOME
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin'  >> .bash_profile
+
+source .bash_profile
+
+# cd into hadoop location
+cd opt/hadoop-2.7.3/etc/hadoop
+
+echo "Adding configurations..."
+
+# update settings in hadoop-env.sh file
+ex="export JAVA_HOME=~/opt/jdk1.8.0_221"
+sed -i "25s@.*@${ex}@" hadoop-env.sh
+
+# Remove empty configuration tags in core-site.xml 
+sed -i "s@<configuration>@$ @g" core-site.xml
+sed -i "s@</configuration>@$ @g" core-site.xml
+
+# Add config settings to core-site.xml
+echo '
+<configuration>
+  <property>
+    <name>fs.default.name</name>
+    <value>hdfs://localhost:9000</value>
+  </property>
+</configuration>' >> core-site.xml
+
+
+# Remove empty configuration tags in the hdfs-site.xml 
+sed -i "s@<configuration>@$ @g" hdfs-site.xml
+sed -i "s@</configuration>@$ @g" hdfs-site.xml
+
+# update hdfs-site.xml config settings
+echo '
+<configuration>
+  <property>
+    <name>dfs.replication</name>
+    <value>1</value>
+  </property>
+  <property>
+    <name>dfs.name.dir</name>
+    <value>file://~/opt/hadoop-2.7.3/hdfs/namenode</value>
+  </property>
+  <property>
+    <name>dfs.name.dir</name>
+    <value>file://~/opt/hadoop-2.7.3/hdfs/datanode</value>
+  </property>
+</configuration>' >> hdfs-site.xml
+
+
+# Remove empty configuration tags in yarn-site.xml
+sed -i "s@<configuration>@$ @g" yarn-site.xml
+sed -i "s@</configuration>@$ @g" yarn-site.xml
+
+# update yarn-site.xml config settings
+echo '
+<configuration>
+  <property>
+    <name>yarn.nodemanager.aux-services</name>
+    <value>mapreduce_shuffle</value>
+  </property>
+</configuration>' >> yarn-site.xml
+
+
+# Make a copy of mapred-site.xml.template:
+cp mapred-site.xml.template mapred-site.xml
+
+# Remove empty configuration tags in mapred-site.xml
+sed -i "s@<configuration>@$ @g" mapred-site.xml
+sed -i "s@</configuration>@$ @g" mapred-site.xml
+
+# update mapred-site.xml config settings
+echo '
+<configuration>
+  <property>
+    <name>mapreduce.framework.name</name>
+    <value>yarn</value>
+  </property>
+</configuration>' >> mapred-site.xml
+
+# Install SSH
+sudo apt-get install openssh-server
+
+# Generate the SSH Key
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+
+# Format namenode
+hdfs namenode -format
+
+# Source the .bash_profile
+source ~/.bash_profile
+
+echo '
+Hadoop successfully installed'
